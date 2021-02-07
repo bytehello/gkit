6f9fe6af709e302a8590f9c8cca7998efc783add
gechanghang <gechanghang@gmail.com>
2021-02-08 01:21:38 +0800

    整理了问题

--- README.md
+++ README.md
@@ -19,3 +19,7 @@ kit 库必须具有的特点：
 可以传入配置文件，使用配置文件中的打印，如果没有指定，那么就选择第一个文件来打印（看看logrus_meta是如何选择的）
 
 可以准入配置文件，指定具体的配置打印
+
+可以动态新增 【携带配置文件的】 logger对象
+
+对应 another_log_test.go 中的 TestLogMate TestHijactLoggerByMate 这两个方法好好想一想如何组织代码
---
+++ another_log_test.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	nlog "log"
+	"testing"
+
+	"github.com/gogap/logrus_mate"
+	"github.com/sirupsen/logrus"
+)
+
+// 创建一个新的logger
+// 使用场景：需要在特定的文件夹打印的情况
+// 这里返回的是logrus.Logger 对象
+// TODO: gkit可以对其封装，返回一个gkit对应的日志库对象
+
+func TestLogMate(t *testing.T) {
+	mate, _ := logrus_mate.NewLogrusMate(
+		logrus_mate.ConfigString( // 这里使用的是一种特殊的数据格式，号称是更加容易读
+			`{ mike {formatter.name = "json" }`,
+		),
+	)
+	mikeLogger := mate.Logger("mike")
+	mikeLogger.Errorln("With JSON MIKE")
+}
+
+// 劫持使用，将logrus.Logger的行为替换掉，自己的项目中有这种情况没？
+// TODO 有这种需求可以，封装
+// mike 代表的配置劫持了StandardLogger
+func TestHijactLoggerByMate(t *testing.T) {
+	mate, _ := logrus_mate.NewLogrusMate(
+		logrus_mate.ConfigString(
+			`{ mike {formatter.name = "json"} }`,
+		),
+	)
+	mate.Hijack(
+		logrus.StandardLogger(),
+		"mike",
+	)
+	logrus.Println("hello std logger is hijack by mike")
+}
+
+// mike 代表的配置劫持了 原生 log
+// NOTICE :这个是不被允许的，只能劫持logrus.Logger
+func TestHijactNativeLoggerByMate(t *testing.T) {
+	mate, _ := logrus_mate.NewLogrusMate(
+		logrus_mate.ConfigString(
+			`{ mike {formatter.name = "json"} }`,
+		),
+	)
+	mate.Hijack(
+		logrus.StandardLogger(),
+		"mike",
+	)
+	logrus.Println("hello std logger is hijack by mike")
+}
+
+func TestHijackLoggerOverwriteByMate(t *testing.T) {
+	mate, _ := logrus_mate.NewLogrusMate(
+		logrus_mate.ConfigString(
+			`{ mike {formatter.name = "json"} }`,
+		),
+		// 读取文件覆盖configString
+		logrus_mate.ConfigFile("mate.conf"),
+	)
+	mate.Hijack(
+		logrus.StandardLogger(),
+		"mike",
+	)
+	logrus.Println("hello std logger is hijack by mike")
+}
+
+func TestLog(t *testing.T) {
+	// t.Log("log")
+	logrus.SetLevel(logrus.TraceLevel)
+	logrus.SetReportCaller(true) // 输出文件名
+	logrus.Trace("trace msg")
+	logrus.Debug("debug msg")
+	logrus.Info("Info msg")
+	logrus.Warning("Warning msg")
+	logrus.Error("Error msg")
+}
+
+func TestFiledsLog(t *testing.T) {
+	newLogger := logrus.WithFields(logrus.Fields{
+		"ip":   "127.0.0.1",
+		"port": 10013,
+	}) // 新建一个携带固定内容的
+	newLogger.Info("info test1 ")
+	newLogger.Info("info test2 ")
+}
+
+func TestLogrusDesign(t *testing.T) {
+	logrus.SetReportCaller(false)                // 配置可用
+	logrus.SetFormatter(&logrus.JSONFormatter{}) // 配置日志格式（logstash可用）
+	logrus.Info("aaaaaa")
+}
+
+type AppHook struct {
+	AppName string
+}
+
+func (ah *AppHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (ah *AppHook) Fire(entry *logrus.Entry) error {
+	// 这个方法在打印日志前调用
+	entry.Data["app"] = ah.AppName
+	return nil
+}
+
+// 设置 hook
+func TestHook(t *testing.T) {
+	h := &AppHook{AppName: "HelloWorldApp"}
+	logrus.AddHook(h)
+	logrus.Debug("HOOK DEBUG")
+}
